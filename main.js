3ff04070fcd6defc656ddf0d5c37a1fe0bb84974
Vyas gv <73235709+Vyas120@users.noreply.github.com>
2021-01-08 19:38:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+console.log("my name is...")
+var name_1 = 'Vyas';
+var name_2 = 'Venkata';
+
+function concat() {
+    concat(name_1, name_2);
+}
+
+
+var number_1 = '4';
+var number_2 = '3';
+
+var join_num = number_1 + number_2;
+console.log(join_num);
+
+var join_num2 = number_1 - number_2;
+console.log(join_num2);
+
+var join_num3 = number_1 * number_2;
+console.log(join_num3);
+
+var join_num4 = number_1 / number_2;
+console.log(join_num4)
